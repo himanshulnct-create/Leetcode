2a0f4db2559de5c993602663f0c17ce85eb0a900
himanshulnct-create <himanshu.iitian2@gmail.com>
2026-09-23 05:45:58 +0530

    Time: 424 ms (61.85%), Space: 273.7 MB (18.38%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,55 +1,16 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        int L = 10;
         int n = s.length();
-
-        if (n <= 10) {
-            return new ArrayList<>();
-        }
-
-        Set<Integer> seen = new HashSet<>();
-        Set<Integer> repeated = new HashSet<>();
-
-        int code = 0;
-        int mask = (1 << 20) - 1;
-
-        // First 10 characters
-        for (int i = 0; i < 10; i++) {
-            code = (code << 2) | encode(s.charAt(i));
-        }
-
-        seen.add(code);
-
-        // Rolling window
-        for (int i = 10; i < n; i++) {
-            code = ((code << 2) | encode(s.charAt(i))) & mask;
-
-            if (!seen.add(code)) {
-                repeated.add(code);
+        HashSet <String> seen = new HashSet<>();
+        HashSet<String> repeated = new HashSet<>();
+        for(int i = 0;i<=(n-L);i++){
+            String seq = s.substring(i, i + L);
+            if(!seen.add(seq)){
+                repeated.add(seq);
             }
         }
-
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i <= n - 10; i++) {
-            int current = 0;
-
-            for (int j = i; j < i + 10; j++) {
-                current = (current << 2) | encode(s.charAt(j));
-            }
-
-            if (repeated.contains(current)) {
-                result.add(s.substring(i, i + 10));
-                repeated.remove(current);
-            }
-        }
-
-        return result;
-    }
-
-    private int encode(char c) {
-        if (c == 'A') return 0;
-        if (c == 'C') return 1;
-        if (c == 'G') return 2;
-        return 3;
+        ArrayList<String> list = new ArrayList<>(repeated);
+        return list;
     }
 }
